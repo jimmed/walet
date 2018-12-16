9cd59e35c79e4e925756f34d317129587c9aa144
Jim O'Brien <jim@jimmed.net>
2018-12-16 21:20:04 +0000

    Allow specifying backend in CLI (wal or vibrant)

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,12 +1,37 @@
-import { WalColourSchemeBackend } from "@walet/core";
+import {
+  WalColourSchemeBackend,
+  VibrantColourSchemeBackend
+} from "@walet/core";
 import { runCli } from "./cli";
+import * as yargs from "yargs";
 
-const backend = new WalColourSchemeBackend();
+const backends = {
+  wal: WalColourSchemeBackend,
+  vibrant: VibrantColourSchemeBackend
+};
 
-const { argv } = process;
-
-const [urlOrPath] = argv.slice(-1);
-
-const light = ["-l", "--light"].some(x => argv.includes(x));
-
-runCli(urlOrPath, light, backend);
+const { argv } = yargs(process.argv).command({
+  command: ["generate", "$0"],
+  describe: "Generate a colour scheme from an image",
+  builder: (yargs: yargs.Argv) =>
+    yargs
+      .option("image", {
+        alias: ["url", "path"],
+        type: "string",
+        describe: "The URL or path of the image"
+      })
+      .option("light", {
+        type: "boolean",
+        default: false,
+        describe: "Generate a light theme (instead of dark)"
+      })
+      .option("backend", {
+        type: "string",
+        choices: Object.keys(backends),
+        default: "vibrant"
+      })
+      .demandOption("image"),
+  handler: ({ image, light, backend }: yargs.Arguments) => {
+    runCli(image, light, new backends[backend]());
+  }
+});
